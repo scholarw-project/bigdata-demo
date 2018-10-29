2689eaa41f2554dbda921ba02db3daf01519294d
scholarw <scholarw@hotmail.com>
2018-10-29 10:39:41 +0800

    init

---
+++ bigdataDemo/src/main/java/cn/wsg/bigdataDemo/App.java
@@ -0,0 +1,13 @@
+package cn.wsg.bigdataDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
