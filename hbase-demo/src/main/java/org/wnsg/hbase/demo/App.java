9a22b5e262c3210c39b346c9eaca89d1694ae3c3
scholarw <scholarw@hotmail.com>
2018-10-29 11:54:58 +0800

    add some maven module for hadoop or realstream demo

---
+++ hbase-demo/src/main/java/org/wnsg/hbase/demo/App.java
@@ -0,0 +1,13 @@
+package org.wnsg.hbase.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
