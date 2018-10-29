adc84ceff53ab6fc12f7d4d426cdd9b6380ccb4b
scholarw <scholarw@hotmail.com>
2018-10-29 11:46:30 +0800

    init

---
+++ hdfs-demo/src/main/java/org/wnsg/demo/hdfs/demo/App.java
@@ -0,0 +1,13 @@
+package org.wnsg.demo.hdfs.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
