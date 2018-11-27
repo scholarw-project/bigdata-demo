0fc68fb4160815d7bd321577b53702bb32f98ec7
wangsg <wangsg@172.16.64.165>
2018-11-27 15:53:53 +0800

    add spark demo

--- kafka-demo/src/main/java/org/wnsg/kafka/demo/App.java
+++ kafka-demo/src/main/java/org/wnsg/kafka/demo/App.java
@@ -1,5 +1,7 @@
 package org.wnsg.kafka.demo;
 
+import kafka.admin.ConsumerGroupCommand.ConsumerGroupService;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        ConsumerGroupService cgs = null;
     }
 }
